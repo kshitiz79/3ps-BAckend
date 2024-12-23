6e097efb73f3a1b26649d1225300925977728fd5
kshitiz79 <kshitiz.21b0131170@abes.ac.in>
2024-12-23 18:39:29 +0530

    fff

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json({ limit: "55mb" }));
 app.use(express.urlencoded({ limit: "55mb", extended: true })); // Use 'extended: true' here
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // Adjust based on your frontend environment
-  credentials: true
+  origin: '*', // Allow all origins
+  credentials: true,
 }));
 
 
